9216748d269a789a63d8eeec5344bb2e55ea1202
유병헌 <forybh@naver.com>
2020-06-11 20:18:52 +0900

    자가진단테스트

--- DigitalMedic/app/src/main/java/com/example/digitalmedic/DementiaResult.java
+++ DigitalMedic/app/src/main/java/com/example/digitalmedic/DementiaResult.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class DementiaResult extends AppCompatActivity {
 
     private Button btn_back;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dementia_result);
+
+        Intent intent = getIntent();
+        int result = intent.getIntExtra("result", 0);
 
         btn_back =(Button)findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +29,8 @@ public class DementiaResult extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tv_result =(TextView)findViewById(R.id.tv_result);
+        tv_result.setText(String.valueOf(result));
     }
 }
