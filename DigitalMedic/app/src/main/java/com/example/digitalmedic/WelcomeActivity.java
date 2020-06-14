ee63d5e7e5c840d0f553c2ecffb52eaca87304e0
CYJ1 <yungie22287@gmail.com>
2020-06-15 01:34:38 +0900

    Edit xml, Add png
    Edit xml, Add png

--- DigitalMedic/app/src/main/java/com/example/digitalmedic/WelcomeActivity.java
+++ DigitalMedic/app/src/main/java/com/example/digitalmedic/WelcomeActivity.java
@@ -32,6 +32,7 @@ class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
 
         // 처음 실행할 때만 뜨도록 설정
         session = new Session(this);
@@ -43,8 +44,6 @@ class WelcomeActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
-
-        setContentView(R.layout.activity_welcome);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
