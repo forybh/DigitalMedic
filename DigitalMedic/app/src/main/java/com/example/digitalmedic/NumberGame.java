acb21278b7fb2e5aaea032664488f840facc7a7c
CYJ1 <yungie22287@gmail.com>
2020-06-18 02:48:14 +0900

    Finish NumberGame
    Finish NumberGame

--- DigitalMedic/app/src/main/java/com/example/digitalmedic/NumberGame.java
+++ DigitalMedic/app/src/main/java/com/example/digitalmedic/NumberGame.java
@@ -1,20 +1,32 @@
 package com.example.digitalmedic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-class NumberGame extends AppCompatActivity {
+public class NumberGame extends AppCompatActivity {
+    private Button btn_back;
     int array[] = new int[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_number);
+
+        btn_back =(Button)findViewById(R.id.btn_back_num);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NumberGame.this, GameLevelNum.class);
+                startActivity(intent);
+            }
+        });
 
         final ImageView iv1 = (ImageView)findViewById(R.id.number_1);
         final ImageView iv2 = (ImageView)findViewById(R.id.number_2);
@@ -34,7 +46,7 @@ class NumberGame extends AppCompatActivity {
             public void onClick(View view) {
                 if(array[0] == 1){
                     array[0] += 1;
-                    game.setText(Integer.toString(array[0]) + "누를 차례입니다.");
+                    game.setText(Integer.toString(array[0]) + "를 누를 차례입니다.");
                 }
             }
         });
@@ -44,7 +56,7 @@ class NumberGame extends AppCompatActivity {
             public void onClick(View view) {
                 if(array[0] == 2){
                     array[0] += 1;
-                    game.setText(Integer.toString(array[0]) + "누를 차례입니다.");
+                    game.setText(Integer.toString(array[0]) + "을 누를 차례입니다.");
                 }
             }
         });
@@ -54,7 +66,7 @@ class NumberGame extends AppCompatActivity {
             public void onClick(View view) {
                 if(array[0] == 3){
                     array[0] += 1;
-                    game.setText(Integer.toString(array[0]) + "누를 차례입니다.");
+                    game.setText(Integer.toString(array[0]) + "를 누를 차례입니다.");
                 }
             }
         });
@@ -64,7 +76,7 @@ class NumberGame extends AppCompatActivity {
             public void onClick(View view) {
                 if(array[0] == 4){
                     array[0] += 1;
-                    game.setText(Integer.toString(array[0]) + "누를 차례입니다.");
+                    game.setText(Integer.toString(array[0]) + "를 누를 차례입니다.");
                 }
             }
         });
@@ -73,7 +85,7 @@ class NumberGame extends AppCompatActivity {
             public void onClick(View view) {
                 if(array[0] == 5){
                     array[0] += 1;
-                    game.setText(Integer.toString(array[0]) + "누를 차례입니다.");
+                    game.setText(Integer.toString(array[0]) + "을 누를 차례입니다.");
                 }
             }
         });
