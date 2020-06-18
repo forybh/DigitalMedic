6d85d64e6677f7d9df9f5c72df0acdc17525c994
유병헌 <forybh@naver.com>
2020-06-18 12:06:56 +0900

    사용법 완료

--- DigitalMedic/app/src/main/java/com/example/digitalmedic/PanicInstruction.java
+++ DigitalMedic/app/src/main/java/com/example/digitalmedic/PanicInstruction.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class DepressInstruction extends AppCompatActivity {
+public class PanicInstruction extends AppCompatActivity {
     private Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_depress_instruction);
+        setContentView(R.layout.activity_panic_instruction);
 
         btn_back =(Button)findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DepressInstruction.this, Depress.class);
+                Intent intent = new Intent(PanicInstruction.this, Panic.class);
                 startActivity(intent);
             }
         });
