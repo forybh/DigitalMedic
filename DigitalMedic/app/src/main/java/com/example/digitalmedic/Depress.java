d50666d6ef1e9b7ddf89f3db70ea4d8f2b7dd719
CYJ1 <yungie22287@gmail.com>
2020-06-19 11:33:26 +0900

    Add ViewFlipper
    Add ViewFlipper to Depress&Panic

--- DigitalMedic/app/src/main/java/com/example/digitalmedic/Depress.java
+++ DigitalMedic/app/src/main/java/com/example/digitalmedic/Depress.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class Depress extends AppCompatActivity {
 
+    private ViewFlipper flipper;
     private Button btn_user;
     private Button btn_alarm;
     private Button btn_test;
@@ -17,13 +22,54 @@ public class Depress extends AppCompatActivity {
     private Button btn_attend;
     private Button btn_chart;
     private Button btn_instruction;
+    private Button btn_prev;
+    private Button btn_next;
+    private ImageView depress_breath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depress);
 
-        btn_user = (Button)findViewById(R.id.btn_user);
+        //ViewFlipper 구현
+        flipper = findViewById(R.id.btn_center);
+        Animation showIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        flipper.setInAnimation(showIn);
+        flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
+        //자동으로 넘어가기
+        flipper.setFlipInterval(2000);
+        flipper.startFlipping();
+
+
+        btn_prev = findViewById(R.id.button5);
+        btn_next = findViewById(R.id.button6);
+        depress_breath = (ImageView)findViewById(R.id.depress_breath);
+
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flipper.showPrevious();
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flipper.showNext();
+            }
+        });
+
+        depress_breath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Depress.this, DepressVideo.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btn_user = findViewById(R.id.btn_user);
         btn_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +78,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_alarm = (Button)findViewById(R.id.btn_notice);
+        btn_alarm = findViewById(R.id.btn_notice);
         btn_alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +87,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_test = (Button)findViewById(R.id.btn_test);
+        btn_test = findViewById(R.id.btn_test);
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,7 +96,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_chatting = (Button)findViewById(R.id.btn_chatting);
+        btn_chatting = findViewById(R.id.btn_chatting);
         btn_chatting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +105,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_video = (Button)findViewById(R.id.btn_video);
+        btn_video = findViewById(R.id.btn_video);
         btn_video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +114,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_attend = (Button)findViewById(R.id.btn_attend);
+        btn_attend = findViewById(R.id.btn_attend);
         btn_attend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,7 +124,7 @@ public class Depress extends AppCompatActivity {
         });
 
 
-        btn_chart = (Button)findViewById(R.id.btn_chart);
+        btn_chart = findViewById(R.id.btn_chart);
         btn_chart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -87,7 +133,7 @@ public class Depress extends AppCompatActivity {
             }
         });
 
-        btn_instruction = (Button)findViewById(R.id.btn_instruction);
+        btn_instruction = findViewById(R.id.btn_instruction);
         btn_instruction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
